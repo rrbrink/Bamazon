8eb4bd22f08575b99ef191bfebe784b21fd48c32
Ryan Brinkerhoff <ryanbrinkerhoff@live.com>
2018-02-01 14:19:33 -0700

    Update BamazonManager.js

--- BamazonManager.js
+++ BamazonManager.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var inquirer = require('inquirer');
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: 8889,
   user: "root",
-  password: "",
+  password: "root",
   database: "Bamazon"
 })
 
